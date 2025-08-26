ee93ba128f462719f92747f3546ccdf9b039f6b9
Marcondes Cunha Araujo <clavem303@gmail.com>
2025-08-26 15:14:20 -0300

    VehicleApi (KAfka implementation).

--- BookingsApi/src/main/java/tech/clavem303/services/clients/VehicleApiClient.java
+++ BookingsApi/src/main/java/tech/clavem303/services/clients/VehicleApiClient.java
@@ -1,11 +1,11 @@
 package tech.clavem303.services.clients;
 
+import io.quarkus.cache.CacheResult;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import tech.clavem303.DTOs.clients.VehicleResponseDTO;
 
@@ -18,5 +18,6 @@ public interface VehicleApiClient {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @CacheResult(cacheName = "vehicles")
     Optional<VehicleResponseDTO> getVehicle(@PathParam("id") Long id);
 }
--- VehiclesApi/src/main/java/tech/clavem303/services/VehicleService.java
+++ VehiclesApi/src/main/java/tech/clavem303/services/VehicleService.java
@@ -1,8 +1,12 @@
 package tech.clavem303.services;
 
+import io.quarkus.cache.CacheInvalidateAll;
+import io.quarkus.cache.CacheResult;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import tech.clavem303.DTOs.VehicleCreateDTO;
 import tech.clavem303.DTOs.VehicleResponseDTO;
 import tech.clavem303.DTOs.VehicleStatusUpdateDTO;
@@ -20,13 +24,21 @@ public class VehicleService {
     @Inject
     VehicleRepository vehicleRepository;
 
+
+    @Inject
+    @Channel("vehicle-updates")
+    Emitter<String> vehicleEmitter;
+
     @Transactional
+    @CacheInvalidateAll(cacheName = "vehicles")
     public VehicleResponseDTO createVehicle(VehicleCreateDTO dto) {
         Vehicle vehicle = new Vehicle(dto.brand(), dto.fabricationYear(), dto.engine(), dto.model());
         vehicleRepository.persist(vehicle);
+        vehicleEmitter.send("New vehicle created with ID: " + vehicle.getId());
         return toResponseDTO(vehicle);
     }
 
+    @CacheResult(cacheName = "vehicles")
     public List<VehicleResponseDTO> findAllVehicles() {
         List<Vehicle> vehicles = vehicleRepository.listAll();
         return toResponseDTOs(vehicles);
@@ -38,25 +50,31 @@ public class VehicleService {
     }
 
     @Transactional
+    @CacheInvalidateAll(cacheName = "vehicles")
     public VehicleResponseDTO updateVehicle(Long id, VehicleUpdateDTO dto) {
         Vehicle vehicle = findVehicleOrThrow(id);
         vehicle.updateAttributes(dto);
+        vehicleEmitter.send("Updated ID " + id + " vehicle.");
         return toResponseDTO(vehicle);
     }
 
     @Transactional
+    @CacheInvalidateAll(cacheName = "vehicles")
     public VehicleResponseDTO updateStatus(Long id, VehicleStatusUpdateDTO dto) {
         Vehicle vehicle = findVehicleOrThrow(id);
         vehicle.changeStatus(dto.status());
+        vehicleEmitter.send("Vehicle status with ID " + id + " updated.");
         return toResponseDTO(vehicle);
     }
 
     @Transactional
+    @CacheInvalidateAll(cacheName = "vehicles")
     public void deleteVehicleById(Long id) {
         boolean deleted = vehicleRepository.deleteById(id);
         if (!deleted) {
             throw new ResourceNotFoundException("Vehicle with ID " + id + "  not found.");
         }
+        vehicleEmitter.send("Updated ID " + id + " deleted.");
     }
 
     private Vehicle findVehicleOrThrow(Long id) {
